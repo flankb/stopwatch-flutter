f3bc1f4433e75ec661315a13ac203ae921c5c401
boris-home <boris-home@home.com>
2020-05-28 13:15:25 +0800

    Корректировка интерфейса

--- lib/widgets/stopwatch_body.dart
+++ lib/widgets/stopwatch_body.dart
@@ -7,60 +7,77 @@ class StopwatchBody extends StatelessWidget {
 
     return Column(
       children: <Widget>[
-        Container(
-          child: Column(
-            children: <Widget>[
-              Text("00:00", style: TextStyle(
-                fontSize: 14
-              ),),
-              Text("00:00", style: TextStyle(
-                fontSize: 24
-              ),)
-            ],
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16, 32, 0, 0),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "00:00",
+                  style: TextStyle(fontSize: 24),
+                ),
+                Text(
+                  "00:00",
+                  style: TextStyle(fontSize: 34),
+                )
+              ],
+            ),
           ),
         ),
         Expanded(
           child: ListView.builder(
             itemCount: items.length,
             itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                leading: Text(index.toString()),
-                  title: Text(items[index]));
+              return ListTile(leading: Text((index + 1).toString()), title: Text(items[index]));
             },
           ),
         ),
         Row(
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                RawMaterialButton(
-                  child: Padding(padding: EdgeInsets.all(16),
-                    child: Text("Старт"),
+            Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 8, 4, 8),
+                  child: SizedBox(
+                    height: 150,
+                    child: RawMaterialButton(
+                      child: Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Text("Старт", style: TextStyle(
+                          fontSize: 22, color: Colors.white
+                        ),),
+                      ),
+                      onPressed: () {},
+                      fillColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                      ),
+                    ),
                   ),
-                  onPressed: () {},
-                  fillColor: Colors.red,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(4)),
+                )),
+            Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(4, 8, 8, 8),
+                  child: SizedBox(
+                    height: 150,
+                    child: RawMaterialButton(
+                      child: Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Text("Круг", style: TextStyle(
+                            fontSize: 22, color: Colors.black
+                        )),
+                      ),
+                      onPressed: () {},
+                      fillColor: Colors.black12,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                      ),
+                    ),
                   ),
-                )
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                RawMaterialButton(
-
-                  child: Padding(padding: EdgeInsets.all(16),
-                    child: Text("Круг"),
-
-                  ),
-                  onPressed: () {},
-                  fillColor: Theme.of(context).primaryColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(4)),
-                  ),
-                )
-              ],
-            )
+                ))
           ],
         ),
       ],
