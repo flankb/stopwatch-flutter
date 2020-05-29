8941244668a0c2357fe9dab66e0d296fee32d367
boris-home <boris-home@home.com>
2020-05-29 16:40:20 +0800

    Кнопка фильтрации

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:learnwords/fake/fake_data_fabric.dart';
@@ -26,38 +27,72 @@ class HistoryPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("История измерений"),
       ),
-      body: Container(
-        child: ListView.builder(
-          itemCount: data.length,
-          itemBuilder: (BuildContext context, int index) {
-            BaseStopwatchEntity entity = data[index];
+      body: Stack(children: [
+        Container(
+          child: ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (BuildContext context, int index) {
+              BaseStopwatchEntity entity = data[index];
 
-            return InkWell(
-              onLongPress: () => {
-                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
-                  return EntityEditPage(entityType: pageType, entityId: entity.id);
-                }))
-              },
-              onTap: () => {
-                if (pageType == MeasureViewModel)
-                  {
-                    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
-                      return HistoryPage(pageType: LapViewModel, entityId: entity.id);
-                    }))
-                  }
-              },
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-                child: Container(
-                    child: Text(
-                  entity.comment,
-                  style: TextStyle(fontSize: 18),
-                )),
-              ),
-            );
-          },
+              return InkWell(
+                onLongPress: () => {
+                  Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+                    return EntityEditPage(entityType: pageType, entityId: entity.id);
+                  }))
+                },
+                onTap: () => {
+                  if (pageType == MeasureViewModel)
+                    {
+                      Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+                        return HistoryPage(pageType: LapViewModel, entityId: entity.id);
+                      }))
+                    }
+                },
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                  child: Container(
+                      child: Text(
+                    entity.comment,
+                    style: TextStyle(fontSize: 18),
+                  )),
+                ),
+              );
+            },
+          ),
         ),
-      ),
+        Align(
+            alignment: Alignment.bottomCenter,
+
+            child: SizedBox(
+              width: 150,
+              child: RawMaterialButton(
+                onPressed: () {
+
+                },
+                child: Padding(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.filter_list, color: Colors.white,),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        "Фильтр",
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 4.0),
+                ),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                elevation: 2.0,
+                fillColor: Theme.of(context).primaryColor,
+                padding: const EdgeInsets.all(5.0),
+              ),
+            )
+        )
+      ]),
     );
   }
 }
