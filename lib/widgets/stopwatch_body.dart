d665945a24881bcfe0906021276934d81f3f19b4
boris-home <boris-home@home.com>
2020-06-07 10:51:34 +0800

    Рефакторинг

--- lib/widgets/stopwatch_body.dart
+++ lib/widgets/stopwatch_body.dart
@@ -36,26 +36,11 @@ class StopwatchBody extends StatelessWidget {
 
                 //date2.difference(birthday).inDays;
                 builder: (context, snapshot) {
-                  /*final delta1 = snapshot.data != -1
-                    ? DateTime.now().difference(state.measure.lastRestartedOverall).inMilliseconds
-                    : 0; // TODO Костыль!!!!! Переделать на регулярное обновление значений elapsed!
-
-                  final delta2 = snapshot.data != -1
-                      ? DateTime.now().difference(state.measure.lastRestartedLap).inMilliseconds
-                      : 0; // TODO Костыль!!!!! Переделать на регулярное обновление значений elapsed!
-
-                  final overallDifference = state.measure.elapsed + delta1;
-                  final lapDifference = state.measure.elapsedLap + delta2; // TODO Если elapsedLap и elapsed обновлены, здесь возникают неправильные значения
-                   */
-
                   final delta1 = DateTime.now().difference(state.measure.lastRestartedOverall).inMilliseconds;
                   final delta2 =  DateTime.now().difference(state.measure.lastRestartedLap).inMilliseconds;
 
                   final overallDifference = state.measure.elapsed + delta1; // TODO elapsed не сбрасывается
                   final lapDifference = state.measure.elapsedLap + delta2;
-
-                  //final d1 = Duration(milliseconds: state.measure.elapsed + 33 * snapshot.data);
-                  //final d2 = Duration(milliseconds: state.measure.elapsedLap + 33 * snapshot.data); // С кругом не работает, потому что Ticker не сбрасывается
 
                   final d1 = Duration(milliseconds: overallDifference);
                   final d2 = Duration(milliseconds: lapDifference);
@@ -69,16 +54,10 @@ class StopwatchBody extends StatelessWidget {
                         children: <Widget>[
                           Text(
                             TimeDisplayer.format(d2),
-                            //snapshot.data.toString(),
-                            //overallDifference.toString(),
-                            /*measureBloc.state.measure.elapsedTime()[0],*/
                             style: TextStyle(fontSize: 30),
                           ),
                           Text(
-                            //"",
                             TimeDisplayer.formatMills(d2),
-                            //snapshot.data.toString(),
-                            /*measureBloc.state.measure.elapsedTime()[1],*/
                             style: TextStyle(fontSize: 16),
                           )
                         ],
