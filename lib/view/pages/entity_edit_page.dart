cdf024247da6e5fadb07db4963e61c7e7f663034
boris-home <boris-home@home.com>
2020-06-08 20:11:52 +0800

    Виджеты BLoC для формы редактирования

--- lib/view/pages/entity_edit_page.dart
+++ lib/view/pages/entity_edit_page.dart
@@ -1,21 +1,25 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learnwords/bloc/entity_bloc/bloc.dart';
+import 'package:learnwords/model/database_models.dart';
+import 'package:learnwords/models/stopwatch_proxy_models.dart';
+import 'package:learnwords/resources/stopwatch_db_repository.dart';
 
 class EntityEditPage extends StatelessWidget {
   final Type entityType;
   final int entityId;
+  final BaseStopwatchEntity entity;
 
-  const EntityEditPage({Key key, this.entityType, this.entityId}) : super(key: key);
+  const EntityEditPage({Key key, this.entityType, this.entityId, this.entity}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Редактирование измерения"),
-      ),
-      body: EditForm()
-    );
+        appBar: AppBar(
+          title: Text("Редактирование измерения"),
+        ),
+        body: EditForm());
   }
 }
 
@@ -37,41 +41,59 @@ class EditFormState extends State<EditForm> {
   // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>(); // TODO У формы обязательно должен быть ключ!!!
 
+  EntityBloc entityBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  _init() {
+    entityBloc = EntityBloc(StopwatchRepository(MyDatabase()));
+  }
+
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TextFormField(
-
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Поле не может быть пустым!';
-                }
-                return null;
-              },
+      child: BlocProvider(
+        create: (BuildContext context) {
+          return entityBloc;
+        },
+        child: BlocBuilder<EntityBloc, EntityState>(builder: (context, snapshot) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Поле не может быть пустым!';
+                    }
+                    return null;
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      // Validate returns true if the form is valid, or false
+                      // otherwise.
+                      if (_formKey.currentState.validate()) {
+                        // If the form is valid, display a Snackbar.
+                        //Scaffold.of(context).showSnackBar(SnackBar(content: Text('Обработка данных..')));
+                      }
+                    },
+                    child: Text('Сохранить'),
+                  ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: RaisedButton(
-                onPressed: () {
-                  // Validate returns true if the form is valid, or false
-                  // otherwise.
-                  if (_formKey.currentState.validate()) {
-                    // If the form is valid, display a Snackbar.
-                    //Scaffold.of(context).showSnackBar(SnackBar(content: Text('Обработка данных..')));
-                  }
-                },
-                child: Text('Сохранить'),
-              ),
-            ),
-          ],
-        ),
+          );
+        }),
       ),
     );
   }
