c8f2e34f51db61d1916b687e500efc6fe0e104ed
boris-home <boris-home@home.com>
2020-06-11 21:34:40 +0800

    Проверка E-mail

--- lib/view/pages/settings_page.dart
+++ lib/view/pages/settings_page.dart
@@ -1,41 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:preferences/preferences.dart';
+import 'package:validators/validators.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Настройки"),
-      ),
-      body: PreferencePage([
+        appBar: AppBar(
+          title: Text("Настройки"),
+        ),
+        body: PreferencePage([
+          SwitchPreference(
+            'Звук',
+            'sound',
+            defaultVal: true,
+          ),
+          SwitchPreference(
+            'Вибрация',
+            'vibration',
+            defaultVal: true,
+          ),
+          SwitchPreference(
+            'Блокировка экрана',
+            'screen_block',
+            defaultVal: true,
+            desc: "Вкл",
+          ),
+          TextFieldPreference(
+            'E-mail',
+            'email',
+            defaultVal: 'email@example.com',
+            validator: (str) {
+              if (!isEmail(str)) {
+                return "Invalid email";
+              }
 
-        SwitchPreference(
-          'Звук',
-          'sound',
-          defaultVal: true,
-        ),
-        SwitchPreference(
-          'Вибрация',
-          'vibration',
-          defaultVal: true,
-        ),
-        SwitchPreference(
-          'Блокировка экрана',
-          'screen_block',
-          defaultVal: true,
-          desc: "Вкл",
-        ),
-        TextFieldPreference(
-          'E-mail',
-          'email',
-
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          autofocus: false,
-          maxLines: 1,
-        )
-      ])
-    );
+              return null;
+            },
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            autofocus: false,
+            maxLines: 1,
+          )
+        ]));
   }
 }
