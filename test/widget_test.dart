4b8f2c3d6cdadfa1376db93d86033fa7855b605a
boris-home <boris-home@home.com>
2020-07-14 12:14:53 +0800

    Пробный тест виджета

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,44 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:preferences/preferences.dart';
+import 'package:stopwatch/bloc/measure_bloc/measure_bloc.dart';
+import 'package:stopwatch/bloc/measure_bloc/measure_event.dart';
 
 import 'package:stopwatch/main.dart';
+import 'package:stopwatch/resources/stopwatch_db_repository.dart';
+import 'package:stopwatch/service_locator.dart';
+import 'package:stopwatch/util/ticker.dart';
+import 'package:stopwatch/widgets/buttons_bar.dart';
+import 'package:stopwatch/widgets/circular.dart';
+import 'package:stopwatch/widgets/metro_app_bar.dart';
+import 'package:stopwatch/widgets/stopwatch_body.dart';
 
 void main() {
-  /*testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  //MeasureBloc measureBloc;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  setUp(() async {
+     //measureBloc = MeasureBloc(Ticker3(), StopwatchRepository());
+     //setupLocators();
+     //await PrefService.init(prefix: 'pref_'); // Настройки
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  tearDown(() {
+    //measureBloc.close();
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });*/
+  testWidgets('Widget test', (WidgetTester tester) async {
+    final metroAppBar = MetroAppBar(primaryCommands: <Widget>[
+        PrimaryCommand(onPressed: (){}, pic: Icons.comment, tooltip: 'Test')
+    ], secondaryCommands: <SecondaryCommand>[
+      SecondaryCommand(commandName: 'about', onPressed: () {}, child: Text('Hello'))
+    ],
+    );
+
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(CenterCircularWidget());
+
+    final messageFinder = find.text('M');
+    expect(messageFinder, findsNothing);
+  });
 }
