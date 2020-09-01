089ae284cfcdc705ee02ca9197c19fbb015d3267
boris <boris@home.ru>
2020-09-01 13:36:23 +0800

    Удаление лишнего кода

--- lib/bloc/measure_bloc/measure_state.dart
+++ lib/bloc/measure_bloc/measure_state.dart
@@ -17,30 +17,23 @@ abstract class MeasureState extends Equatable {
   List<Object> get props => [version];
 }
 
-//TODO Хорошо бы отрефакторить код, на Generic?
 class MeasureReadyState extends MeasureState {
   MeasureReadyState(MeasureViewModel measure) : super(measure, MeasureState.getUpdatedVersion());
-  //MeasureReadyState.copyFromState(MeasureState base) : super(base.measure, base.getUpdatedVersion());
 }
 
 class MeasureUpdatingState extends MeasureState {
   // Пока можно это состояние присваивать при старте программы перед загрузкой секундомера
   MeasureUpdatingState(MeasureViewModel measure) : super(measure, MeasureState.getUpdatedVersion());
-  //MeasureUpdatingState.copyFromState(MeasureState base) : super(base.measure, base.getUpdatedVersion());
 }
 
 class MeasurePausedState extends MeasureState {
   MeasurePausedState(MeasureViewModel measure) : super(measure, MeasureState.getUpdatedVersion());
-  //MeasurePausedState.copyFromState(MeasureState base) : super(base.measure, base.getUpdatedVersion());
 }
 
 class MeasureStartedState extends MeasureState {
   MeasureStartedState(MeasureViewModel measure) : super(measure, MeasureState.getUpdatedVersion());
-  //MeasureStartedState.copyFromState(MeasureState base) : super(base.measure, base.getUpdatedVersion());
 }
 
 class MeasureFinishedState extends MeasureState {
   MeasureFinishedState(MeasureViewModel measure) : super(measure, MeasureState.getUpdatedVersion());
-  //MeasureFinishedState.copyFromState(MeasureState base) : super(base.measure, base.getUpdatedVersion());
 }
-
