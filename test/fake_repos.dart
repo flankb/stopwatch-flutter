6342dfb493fbbcaa2a29b55d0aa0775923adadec
boris <boris@home.ru>
2021-10-21 14:23:26 +0800

    migrate test

--- test/fake_repos.dart
+++ test/fake_repos.dart
@@ -1,6 +1,7 @@
 // Fake class
 import 'dart:math';
 
+import 'package:drift/drift.dart';
 import 'package:flutter/foundation.dart';
 import 'package:stopwatch/fake/fake_data_fabric.dart';
 import 'package:stopwatch/model/database_models.dart';
@@ -8,6 +9,7 @@ import 'package:stopwatch/models/stopwatch_proxy_models.dart';
 import 'package:stopwatch/models/stopwatch_status.dart';
 import 'package:stopwatch/resources/stopwatch_db_repository.dart';
 import 'package:mockito/mockito.dart';
+import 'package:collection/collection.dart';
 
 class StopwatchFakeRepository extends Fake implements StopwatchRepository {
   late List<MeasureViewModel> _measures;
@@ -31,15 +33,72 @@ class StopwatchFakeRepository extends Fake implements StopwatchRepository {
       {int? limit}) async {
     return _measures
         .where((element) => describeEnum(element.status) == status)
-        .map((e) => e.toEntity())
+        .map((e) => _convertToMeasure(e))
         .toList();
+  }
+
+  // TODO Refactor this methods!
+  Measure _convertToMeasure(MeasureViewModel measureViewModel) {
+    return Measure(
+        id: measureViewModel.id!,
+        comment: measureViewModel.comment,
+        status: (describeEnum(measureViewModel.status)),
+        dateStarted: measureViewModel.dateStarted,
+        elapsed: measureViewModel.elapsed);
+  }
+
+  Lap _convertToLap(LapViewModel lapViewModel) {
+    return Lap(
+        id: lapViewModel.id!,
+        measureId: lapViewModel.measureId,
+        order: lapViewModel.order,
+        difference: lapViewModel.difference,
+        comment: lapViewModel.comment,
+        overall: lapViewModel.overall);
+  }
+
+  MeasureSession _convertToMeasureSession(MeasureSessionViewModel msc) {
+    return MeasureSession(
+        id: msc.id!,
+        measureId: msc.measureId,
+        startedOffset: msc.startedOffset,
+        finishedOffset: msc.finishedOffset);
+  }
+
+  MeasureViewModel _convertToMeasureViewModel(MeasuresCompanion mc) {
+    return MeasureViewModel(
+        id: mc.id.value,
+        comment: mc.comment.value,
+        status: StopwatchStatus.values
+            .firstWhere((es) => describeEnum(es) == mc.status.value),
+        dateStarted: mc.dateStarted.value,
+        elapsed: mc.elapsed.value);
+  }
+
+  MeasureSessionViewModel _convertToMeasureSessionViewModel(
+      MeasureSessionsCompanion measureSession) {
+    return MeasureSessionViewModel(
+        id: measureSession.id.value,
+        measureId: measureSession.measureId.value,
+        startedOffset: measureSession.startedOffset.value,
+        finishedOffset: measureSession.finishedOffset.value);
+  }
+
+  LapViewModel _convertToLapViewModel(LapsCompanion lap) {
+    return LapViewModel(
+        id: lap.id.value,
+        measureId: lap.measureId.value,
+        order: lap.order.value,
+        difference: lap.difference.value,
+        comment: lap.comment.value,
+        overall: lap.overall.value);
   }
 
   @override
   Future<Measure> getMeasuresByIdAsync(int id) async {
-    return _measures
-        .firstWhere((element) => element.id == id, orElse: () => null)
-        .toEntity();
+    final measureViewModel =
+        _measures.firstWhereOrNull((element) => element.id == id);
+    return _convertToMeasure(measureViewModel!);
   }
 
   @override
@@ -56,67 +115,71 @@ class StopwatchFakeRepository extends Fake implements StopwatchRepository {
   }
 
   @override
-  Future<int> addNewLapAsync(Lap lap) async {
-    final lapViewModel = LapViewModel.fromEntity(lap);
+  Future<int> addNewLapAsync(Insertable<Lap> lap) async {
+    final lapViewModel = _convertToLapViewModel(lap as LapsCompanion);
     lapViewModel.id = Random(50).nextInt(2000) + 100;
 
     _laps.add(lapViewModel);
-    return lapViewModel.id;
+    return lapViewModel.id!;
   }
 
   @override
-  Future<int> addNewMeasureSession(MeasureSession measureSession) async {
-    MeasureSessionViewModel session =
-        MeasureSessionViewModel.fromEntity(measureSession);
+  Future<int> addNewMeasureSession(
+      Insertable<MeasureSession> measureSession) async {
+    MeasureSessionViewModel session = _convertToMeasureSessionViewModel(
+        measureSession as MeasureSessionsCompanion);
     session.id = Random(55).nextInt(2000) + 100;
 
     sessions.add(session);
-    return session.id;
+    return session.id!;
   }
 
   @override
   Future<List<MeasureSession>> getMeasureSessions(int measureId) async {
     return sessions
         .where((element) => element.measureId == measureId)
-        .map((e) => e.toEntity())
+        .map((e) => _convertToMeasureSession(e))
         .toList();
   }
 
   @override
-  Future updateMeasureAsync(Measure measure) async {
-    final measureForUpdate =
-        _measures.firstWhere((element) => element.id == measure.id);
+  Future updateMeasureAsync(Insertable<Measure> measure) async {
+    final measureForUpdate = _measures.firstWhere(
+        (element) => element.id == (measure as MeasuresCompanion).id.value);
     _measures.remove(measureForUpdate);
 
-    final viewModel = MeasureViewModel.fromEntity(measure);
+    final viewModel = _convertToMeasureViewModel(measure as MeasuresCompanion);
     debugPrint("Converted from entity: " + viewModel.toString());
 
     _measures.add(viewModel);
   }
 
   @override
-  Future<bool> updateMeasureSession(MeasureSession measureSession) async {
-    final sessionForUpdate =
-        sessions.firstWhere((element) => element.id == measureSession.id);
+  Future<bool> updateMeasureSession(
+      Insertable<MeasureSession> measureSession) async {
+    final sessionForUpdate = sessions.firstWhere((element) =>
+        element.id == (measureSession as MeasureSessionsCompanion).id.value);
     sessions.remove(sessionForUpdate);
 
-    sessions.add(MeasureSessionViewModel.fromEntity(measureSession));
+    sessions.add(_convertToMeasureSessionViewModel(
+        measureSession as MeasureSessionsCompanion));
     return true;
   }
 
   @override
-  Future updateLapAsync(Lap lap) async {
-    final lapForUpdate = _laps.firstWhere((element) => element.id == lap.id);
+  Future updateLapAsync(Insertable<Lap> lap) async {
+    final lapForUpdate = _laps
+        .firstWhere((element) => element.id == (lap as LapsCompanion).id.value);
     _laps.remove(lapForUpdate);
 
-    _laps.add(LapViewModel.fromEntity(lap));
+    _laps.add(_convertToLapViewModel(lap as LapsCompanion));
   }
 
   @override
   Future<List<Lap>> getLapsByMeasureAsync(int measureId) async {
     return _laps
         .where((element) => element.measureId == measureId)
-        .map((e) => e.toEntity())
+        .map((e) => _convertToLap(e))
         .toList();
   }
 
