626d3913201dcb04dcce052dfaa0fc5d1c364dd6
boris <boris@home.ru>
2021-11-21 17:12:45 +0800

    fix linter warnings

--- lib/models/measure_view_model.dart
+++ lib/models/measure_view_model.dart
@@ -1,13 +1,13 @@
+import 'package:collection/collection.dart';
 import 'package:drift/drift.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:stopwatch/model/database_models.dart';
+import 'package:stopwatch/util/math_helper.dart';
 import 'package:stopwatch/util/time_displayer.dart';
 
 import 'stopwatch_proxy_models.dart';
 import 'stopwatch_status.dart';
-import 'package:stopwatch/util/math_helper.dart';
-import 'package:collection/collection.dart';
 
 @immutable
 class MeasureViewModel extends BaseStopwatchEntity {
@@ -21,10 +21,11 @@ class MeasureViewModel extends BaseStopwatchEntity {
   /// Вспомогательное свойство для динамического расчета истекшего времени
   final DateTime lastRestartedOverall; //DateTime.now()
 
-  @deprecated
-  final int checkPointLapTicks; //0
+  // @deprecated
+  // final int checkPointLapTicks; //0
 
   MeasureViewModel({
+    required this.lastRestartedOverall,
     int? id,
     String? comment,
     this.elapsed = 0,
@@ -33,22 +34,22 @@ class MeasureViewModel extends BaseStopwatchEntity {
     this.sessions = const <MeasureSessionViewModel>[],
     this.status = StopwatchStatus.Ready,
     this.dateStarted,
-    required this.lastRestartedOverall,
-    this.checkPointLapTicks = 0,
+    //this.checkPointLapTicks = 0,
   }) : super(id: id, comment: comment);
 
-  MeasureViewModel copyWith(
-      {int? id,
-      String? comment,
-      int? elapsed,
-      int? elapsedLap,
-      List<LapViewModel>? laps,
-      List<MeasureSessionViewModel>? sessions,
-      StopwatchStatus? status,
-      DateTime? dateCreated,
-      DateTime? lastRestartedOverall,
-      int? checkPointLapTicks}) {
-    return MeasureViewModel(
+  MeasureViewModel copyWith({
+    int? id,
+    String? comment,
+    int? elapsed,
+    int? elapsedLap,
+    List<LapViewModel>? laps,
+    List<MeasureSessionViewModel>? sessions,
+    StopwatchStatus? status,
+    DateTime? dateCreated,
+    DateTime? lastRestartedOverall,
+    int? checkPointLapTicks,
+  }) =>
+      MeasureViewModel(
         id: id ?? this.id,
         comment: comment ?? this.comment,
         elapsed: elapsed ?? this.elapsed,
@@ -56,40 +57,35 @@ class MeasureViewModel extends BaseStopwatchEntity {
         laps: laps ?? List.from(this.laps),
         sessions: sessions ?? List.from(this.sessions),
         status: status ?? this.status,
-        dateStarted: dateCreated ?? this.dateStarted,
+        dateStarted: dateCreated ?? dateStarted,
         lastRestartedOverall: lastRestartedOverall ?? this.lastRestartedOverall,
-        checkPointLapTicks: checkPointLapTicks ?? this.checkPointLapTicks);
-  }
+        //checkPointLapTicks: checkPointLapTicks ?? this.checkPointLapTicks
+      );
 
-  List<String> elapsedTime() {
-    return [
-      TimeDisplayer.formatBase(Duration(milliseconds: elapsed)),
-      TimeDisplayer.formatMills(Duration(milliseconds: elapsed))
-    ];
-  }
+  List<String> elapsedTime() => [
+        TimeDisplayer.formatBase(Duration(milliseconds: elapsed)),
+        TimeDisplayer.formatMills(Duration(milliseconds: elapsed))
+      ];
 
-  List<String> elapsedTimeLap() {
-    return [
-      TimeDisplayer.formatBase(Duration(milliseconds: elapsedLap)),
-      TimeDisplayer.formatMills(Duration(milliseconds: elapsedLap))
-    ];
-  }
+  List<String> elapsedTimeLap() => [
+        TimeDisplayer.formatBase(Duration(milliseconds: elapsedLap)),
+        TimeDisplayer.formatMills(Duration(milliseconds: elapsedLap))
+      ];
 
-  int getElapsedSinceStarted(DateTime dateNow) {
-    return dateNow.difference(dateStarted!).inMilliseconds;
-  }
+  int getElapsedSinceStarted(DateTime dateNow) =>
+      dateNow.difference(dateStarted!).inMilliseconds;
 
   /// Вычислить все прошедшее время измерения
   int getSumOfElapsed(DateTime dateNow) {
     var elapsed = 0;
 
-    debugPrint("All Sessions: ");
-    sessions.forEach((element) {
+    debugPrint('All Sessions: ');
+    for (final element in sessions) {
       debugPrint(element.toString());
-    });
-    sessions.where((element) => element.finishedOffset != null).forEach((s) =>
-        elapsed += (s.finishedOffset! -
-            s.startedOffset)); // TODO При добавлении круга здесь ошибка!
+    }
+    sessions.where((element) => element.finishedOffset != null).forEach(
+          (s) => elapsed += s.finishedOffset! - s.startedOffset,
+        ); // TODO При добавлении круга здесь ошибка!
 
     final lastUnfinishedSession = getLastUnfinishedSession();
 
@@ -118,42 +114,37 @@ class MeasureViewModel extends BaseStopwatchEntity {
   }
 
   MeasureSessionViewModel? getLastUnfinishedSession() {
-    // TODO Спорный момент с сортировкой!
-    //sessions.sort((a, b) => a.startedOffset.compareTo(b.startedOffset));
-
     if (sessions.where((element) => element.finishedOffset == null).length >
         1) {
-      throw  Exception(
-          'Обнаружено более одной измерительной сессии с открытым окончанием!');
+      throw Exception(
+        'Обнаружено более одной измерительной сессии с открытым окончанием!',
+      );
     }
 
     return sessions.lastWhereOrNull((s) => s.finishedOffset == null);
   }
 
-  MeasuresCompanion toEntity() {
-    return MeasuresCompanion(
-        id: id != null ? Value<int>(id!) : Value.absent(),
+  MeasuresCompanion toEntity() => MeasuresCompanion(
+        id: id != null ? Value<int>(id!) : const Value.absent(),
         comment: Value(comment),
         status: Value(describeEnum(status)),
         dateStarted: Value(dateStarted),
-        elapsed: Value(elapsed));
-  }
+        elapsed: Value(elapsed),
+      );
 
-  static MeasureViewModel fromEntity(Measure entity) {
-    return MeasureViewModel(
+  factory MeasureViewModel.fromEntity(Measure entity) => MeasureViewModel(
         id: entity.id,
         comment: entity.comment,
         status: StopwatchStatus.values
             .firstWhere((e) => describeEnum(e) == entity.status),
         dateStarted: entity.dateStarted,
         elapsed: entity.elapsed,
-        lastRestartedOverall: DateTime.now());
-  }
+        lastRestartedOverall: DateTime.now(),
+      );
 
   @override
-  String toString() {
-    return 'MeasureViewModel{id: $id, comment: $comment,  elapsed: $elapsed, elapsedLap: $elapsedLap, dateCreated: $dateStarted, status: $status}';
-  }
+  String toString() =>
+      'MeasureViewModel id: $id, comment: $comment,  elapsed: $elapsed, elapsedLap: $elapsedLap, dateCreated: $dateStarted, status: $status';
 
   @override
   List<Object?> get props => super.props
